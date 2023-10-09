75a3ae0963f9c9b7f9af1066265ecbd1de6c5b60
Varad Deshmukh <varaddeshmukh18@gmail.com>
2023-10-10 01:50:02 +0530

    Initial commit

---
+++ app/src/main/java/com/vaddy/calculatorapp/CalculatorOperation.kt
@@ -0,0 +1,9 @@
+package com.vaddy.calculatorapp
+
+sealed class CalculatorOperation(val symbol: String) {
+    object Add: CalculatorOperation("+")
+    object Subtract: CalculatorOperation("-")
+    object Multiply: CalculatorOperation("×")
+    object Divide: CalculatorOperation("÷")
+}
+
